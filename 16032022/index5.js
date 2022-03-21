4a41fdcb469c8748a4759e8076d5e4e7b7e51009
Icefrut <victoria.bagacheva@yandex.com>
2022-03-21 17:02:35 +0200

    homework

---
+++ 16032022/index5.js
@@ -0,0 +1,9 @@
+function FunctionName() {
+  document.body.style.backgroundColor = "white";
+  let x =document.getElementById("mode");
+  x.style.color = "black";
+}
+function FunctionName2() {
+  document.body.style.backgroundColor = "#333952";
+  let x =document.getElementById("mode");
+  x.style.color = "white";}
