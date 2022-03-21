24e2a474a68cc0be8e16a9730ad4f1f9d5662410
Icefrut <victoria.bagacheva@yandex.com>
2022-03-21 17:49:18 +0200

    Math

---
+++ 21032022/index1.js
@@ -0,0 +1,8 @@
+function domath() {
+  // document.getElementById("calculated").innerHTML = Math.round(6.7);
+  // document.getElementById("calculated").innerHTML = Math.ceil(6.3);
+  // document.getElementById("calculated").innerHTML = Math.floor(6.7);
+  // document.getElementById("calculated").innerHTML = Math.pow(4,2);
+  // document.getElementById("calculated").innerHTML = Math.sqrt(25);
+  document.getElementById("calculated").innerHTML = Math.round(Math.random() *201);
+}
